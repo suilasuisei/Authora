1345c154c34de597b35f975ee84023ffad236eee
MARS\yoyo2 <yoyo200304235@gmail.com>
2026-08-11 23:17:49 +0800

    fix; 前端套件修正

---
+++ project0806/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen  flex items-center justify-center">
+      <h1 className="text-4xl font-bold" >
+        好愛...王月...
+      </h1>
+      <div> 
+        
+      </div>
+    </main>
+  );
+}
