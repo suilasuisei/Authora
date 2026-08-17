467f400d35cd325598cfd4bf0120b646dd01b094
MARS\yoyo2 <yoyo200304235@gmail.com>
2026-08-17 21:08:22 +0800

    Feat;前端註冊與登入功能開發

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,15 +1,8 @@
 export default function Home() {
   return (
     <main className="min-h-screen  flex items-center justify-center">
-      <h1 className="text-4xl font-bold" >
-        好愛...王月...
-      </h1>
-      <div>
-        <input/>
-        <button className="">
-          登入
-        </button>
-        </div>
+      <h1 className="text-4xl font-bold">好愛...王月...</h1>
+      <div></div>
     </main>
   );
 }
