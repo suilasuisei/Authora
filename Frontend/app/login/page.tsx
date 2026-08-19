1acbe9d9a84ae2ceeb54bac4af2a84f24ba8a6fd
MARS\yoyo2 <yoyo200304235@gmail.com>
2026-08-19 21:11:47 +0800

    fix;更新登入頁面使用自訂輸入元件

--- Frontend/app/login/page.tsx
+++ Frontend/app/login/page.tsx
@@ -10,8 +10,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
@@ -65,11 +65,10 @@ export default function Login() {
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="account">帳號</Label>
-                <input
+                <Input
                   type="text"
                   id="account"
                   placeholder=""
-                  className=" w-full rounded-lg border border-gray-300 px-4 py-2 outline-none transition focus:border-black focus:ring-1 focus:ring-black"
                   value={account}
                   onChange={(e) => setAccount(e.target.value)}
                 />
@@ -84,10 +83,9 @@ export default function Login() {
                     忘記密碼?
                   </a>
                 </div>
-                <input
+                <Input
                   type="password"
                   id="password"
-                  className=" w-full rounded-lg border border-gray-300 px-4 py-2 outline-none transition focus:border-black focus:ring-1 focus:ring-black"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
